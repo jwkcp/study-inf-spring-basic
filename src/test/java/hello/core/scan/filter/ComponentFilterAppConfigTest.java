8638d9cba36876717ae1c38739c9db08854fd4d4
jwkcp <jaewoong.go@gmail.com>
2023-01-25 07:10:49 +0900

    chapter(컴포넌트 스캔): 필터 완료

--- src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
+++ src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -2,6 +2,7 @@ package hello.core.scan.filter;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,14 +14,14 @@ import static org.springframework.context.annotation.ComponentScan.*;
 public class ComponentFilterAppConfigTest {
     @Test
     void filterScan() {
-        ApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class );
+        ApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
 
         BeanA beanA = ac.getBean("beanA", BeanA.class);
         Assertions.assertThat(beanA).isNotNull();
 
-        Ass
-        BeanB beanB = ac.getBean("beanB", BeanB.class);
-        Assertions.assertThat(beanB).isNull();
+        org.junit.jupiter.api.Assertions.assertThrows(
+                NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("beanB", BeanB.class));
     }
 
     @Configuration

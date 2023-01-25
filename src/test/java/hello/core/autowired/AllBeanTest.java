168099062d3c33112562f2f90d90971ddede4dd5
jwkcp <jaewoong.go@gmail.com>
2023-01-25 23:32:47 +0900

    [스프링 핵심 원리 이해2-스프링 컨테이너와 스프링 빈] 조회한 빈이 모두 필요할 때 완료

--- src/test/java/hello/core/autowired/AllBeanTest.java
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -1,7 +1,12 @@
 package hello.core.autowired;
 
+import hello.core.AutoAppConfig;
 import hello.core.discount.DiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -11,7 +16,18 @@ import java.util.Map;
 public class AllBeanTest {
     @Test
     void findAllBean() {
-        ApplicationContext ac = new AnnotationConfigApplicationContext(DiscountService.class);
+        ApplicationContext ac = new AnnotationConfigApplicationContext(
+                AutoAppConfig.class, DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "userA", Grade.VIP);
+        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+
+        Assertions.assertThat(discountService).isInstanceOf(DiscountService.class);
+        Assertions.assertThat(discountPrice).isEqualTo(1000);
+
+        int rateDiscountPolicy = discountService.discount(member, 20000, "rateDiscountPolicy");
+        Assertions.assertThat(rateDiscountPolicy).isEqualTo(2000);
     }
 
     static class DiscountService {
@@ -25,6 +41,12 @@ public class AllBeanTest {
             System.out.println("policyMap = " + policyMap);
             System.out.println("policies = " + policies);
 
+        }
+
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+
+            return discountPolicy.discount(member, price);
         }
     }
 }
